60a1d6eb35bcd880cc8acdbb89c0f50a2a0333ed
chaeyeon0130 <k84292330@gmail.com>
2023-11-29 11:51:04 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/foodmission/FoodmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodmission/FoodmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
